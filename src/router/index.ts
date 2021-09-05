a5a84267134d31a015627fc0a9fbd3b09a3fe3d2
yuqing521 <1581446178@qq.com>
2021-09-05 16:39:24 +0800

    feat: add tab bar

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,36 +7,6 @@ const routes: Array<RouteRecordRaw> = [
         name: 'Home',
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
     },
-    {
-        path: '/about',
-        name: 'About',
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
-    },
-    {
-        path: '/todoList',
-        name: 'TodoList',
-        component: () => import(/* webpackChunkName: "todo" */ '@/views/TodoList.vue')
-    },
-    {
-        path: '/luckdraw',
-        name: 'LuckDraw',
-        component: () => import(/* webpackChunkName: "luckdraw" */ '@/views/LuckDraw.vue')
-    },
-    {
-        path: '/address/list',
-        name: 'Address',
-        component: () => import(/* webpackChunkName: "address" */ '@/views/address/AddressList')
-    },
-    {
-        path: '/address/edit',
-        name: 'AddressEdit',
-        component: () => import(/* webpackChunkName: "address" */ '@/views/address/AddressEdit')
-    },
-    {
-        path: '/chat/list',
-        name: 'Chat',
-        component: () => import(/* webpackChunkName: "chatpage" */ '@/views/chat')
-    }
 ]
 
 const router = createRouter({
