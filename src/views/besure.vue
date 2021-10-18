c95ed16c8e7f148b477ab5d57db0c78380253207
tusake <8463894+tusake@user.noreply.gitee.com>
2021-10-18 19:38:15 +0800

    尝试提交

---
+++ src/views/besure.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="title">您的外卖当前所在柜门号为：</div>
+      <div class="title" style="font-size: 30px">123456</div>
+      <div class="title">是否开箱？</div>
+      <div class="buttons">
+        <el-button class="button" type="primary" @click="sure">确认</el-button>
+      </div>
+      <div class="buttons">
+        <el-button class="button" type="danger" @click="cancel">取消</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+export default defineComponent({
+  name: 'Besure',
+  setup() {
+    const router = useRouter()
+    const sure = () => {
+      axios({
+        url: 'https://www.baidu.com',
+        method: 'post',
+        responseType: 'json',
+      })
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    }
+    const cancel = () => {
+      router.push('/')
+    }
+    return { sure, cancel }
+  },
+})
+</script>
+
+<style scoped>
+.container {
+  padding-top: 30vh;
+}
+.title {
+  text-align: center;
+  padding-bottom: 10px;
+}
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 20px;
+}
+.button {
+  width: 40vw;
+}
+</style>
---
+++ src/views/delivery/Home.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <div class="background">
+      <div class="hello">
+        <div class="title">你好，送餐者：{{ data.user.username }}</div>
+        <div class="title">当前送餐：</div>
+      </div>
+      <div class="todo">
+        <div class="content">
+          <div class="order">订单编号：{{ data.user.todo.oid }}</div>
+          <div class="telephone">手机号：{{ data.user.todo.telephone }}</div>
+          <div class="state">当前状态：{{ data.user.todo.state }}</div>
+        </div>
+      </div>
+      <div class="qrReader">
+        <div class="qrBg" @click="toScan"></div>
+        <div class="qrIcon">
+          <img src="../../assets/scan_icon.png" />
+          <div class="text">请点击此处扫描二维码</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, reactive, watch } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+    const data = reactive({
+      result: '',
+      error: '',
+      user: {
+        username: '小王',
+        todo: {
+          oid: '1487123172',
+          telephone: '156****7452',
+          state: '待取餐',
+        },
+      },
+    })
+    const toScan = () => {
+      router.push('/dScan')
+    }
+    return { data, toScan }
+  },
+})
+</script>
+
+<style scoped>
+* {
+  font-size: 16px;
+}
+.camera {
+  height: 60%;
+  width: 60%;
+  margin: 20px auto;
+}
+.qrReader {
+  text-align: center;
+  margin: 50px 0;
+}
+.background {
+  height: 87vh;
+  width: 100vw;
+  background-image: url('../../assets/background.jpg');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+}
+
+.content {
+  z-index: 1;
+  padding-top: 20px;
+  margin: 20px auto;
+  width: 40vh;
+  text-align: left;
+}
+.hello {
+  padding: 20px;
+}
+.title {
+  font-size: 30px;
+}
+.order,
+.telephone,
+.state {
+  font-size: 25px;
+}
+.qrBg {
+  left: 50%;
+  width: 70vw;
+  height: 40vh;
+  margin-left: -35vw;
+  position: absolute;
+  border-radius: 20px;
+  opacity: 20%;
+  background-color: white;
+}
+.qrIcon {
+  padding-top: 5vh;
+  width: 100vw;
+  height: 30vh;
+}
+.todo {
+  margin: 0 auto;
+  width: 90vw;
+  height: 18vh;
+  border-radius: 20px;
+  opacity: 50%;
+  background-color: white;
+}
+</style>
